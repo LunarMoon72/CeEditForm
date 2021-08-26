79efdcb40003dada66d87e4b2182f48e7a1bcc82
LunarMoon72 <88513736+LunarMoon72@users.noreply.github.com>
2021-08-26 10:59:59 -0400

    Update Main.php

--- src/LunarMoon72/EnchantCarnage/Main.php
+++ src/LunarMoon72/EnchantCarnage/Main.php
@@ -10,96 +10,41 @@ use pocketmine\Server;
 
 class Main extends PluginBase
 {
+	public function onEnabled(){
+		$this->getLogger()->info("Plugin Enabled");
+	}
 	public function onCommand(CommandSender $sender, Command $cmd, String $label, Array $args) : bool {
 		switch($cmd->getName()){
-			case "ecarnage":
-			  if($sender instanceof Player){
-			  	$sender->select($sender);
+			case "cmd":
+			  if(!$sender instanceof Player){
+			  	$this->getLogger()->info("Use this ingame!");			  
 			  } else {
-			  	$this->getLogger()->info("Go ingame to use this command");
+			  	$sender->newfunction($sender);
 			  }
+
 		}
 		return true;
 	}
-    public function select($player){
-    	$form = $this->getServer()->getPluginManager()->getPlugin("FormAPI")->createSimpleForm(function (Player $player, int $data = null){
-    		if($data === null){
-    			return true;
-    		}
-    		switch($data){
-    			case 0:
-    			  $sender->weapon($sender);
+	public function newfunction($player){
+		$api = $this->getServer()->getPluginManager()->getPlugin("FormAPI");
+		$form = $api->createCustomForm(function(Player $player, int $data = null){
+			if($data === null){
+				return true;
+			}
+			switch($data){
+               case 0:
+                  $this->dispatchCommand("rca " . $player . "ce enchant driller " . $data[0]);
+               break;
 
-    			break;
-
-    			case 1:
-    			  $sender->tool($sender);
-
-    			break;
-    		}
-    	});
-    	$form->setTitle("Select a Catagory");
-    	$form->addButton("Weapon");
-    	$form->addButton("Tools");
-    	$form->sendToPlayer($player);
-    	return $form;   
-    }
-    public function weapon($player){
-    	$form = $this->getServer()->getPluginManager()->getPlugin("FormAPI")->createCustomForm(function(Player $player, int $data = null){
-    		if($data === null){
-    			return true;
-    		}
-    		switch($data){
-
-    			case 0:
-    			  $this->getServer()->dispatchCommand($player, "enchant " . $player->getName() . " sharpness " . $data[0]);
-
-    			break;
-
-    			case 1:
-    			  $this->getServer()->dispatchCommand($player, "enchant " . $player->getName() . " unbreaking " . $data[1]);
-    			break;
-
-    			case 2:
-    			  $this->getServer()->dispatchCommand($player, "enchant " . $player->getName() . " knockback " . $data[2]);
-    			break;
-
-    			case 3:
-    			  $this->getServer()->dispatchCommand($player, "enchant " . $player->getName() . " fire_aspect " . $data[3]);
-    			break;
-    		}
-    	});
-    	$form->setTitle("Press an enchant");
-    	$form->addSlider("Sharpness", 0, 10);
-    	$form->addSlider("Unbreaking", 0, 5);
-    	$form->addSlider("KnockBack", 0, 5);
-    	$form->addSlider("Fire Aspect", 0, 5);
-    	$form->sendToPlayer($player);
-    	return $form;
-    }
-    public function tool($player){
-    	$form = $this->getServer()->getPluginManager()->getPlugin("FormAPI")->createCustomForm(function (Player $player, int $data = null){
-    		if($data === null){
-    			return true;
-    		}
-    		switch($data){
-    			case 0:
-    			  $this->getServer()->dispatchCommand($player, "enchant " . $player->getName() . " efficency " . $data[0]);
-    			break;
-
-    			case 1:
-    			  $this->getServer()->dispatchCommand($player, "enchant " . $player->getName() . " unbreaking " . $data[1]);
-    			break;
-
-    			case 2:
-    			  $this->getServer()->dispatchCommand($player, "enchant " . $player->getName() . " fortune " . $data[2]);
-    			break;
-    		}
-    	});
-    	$form->setTitle("Select an Enchant");
-    	$form->addSlider("Efficency", 0, 10);
-    	$form->addSlider("Fortune", 0, 10);
-    	$form->sendToPlayer($player);
-    	return $form;
-    }
+               case 1:
+                  $this->dispatchCommand("rca " . $player . "ce enchant deathbringer " . $data[1]);
+               break;
+			}
+		});
+		$form->setTitle("Select an Enchant");
+		$form->addSlider("Driller", 0, 10);
+		$form->addSlider("Death Bringer", 0, 10);
+		$form->sendToPlayer();
+		return $form;
+	}
 }
